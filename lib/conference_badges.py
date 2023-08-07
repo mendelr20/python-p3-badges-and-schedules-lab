3e916afa9aa90357c8be40e773913946e3762014
mendelr20 <95175747+mendelr20@users.noreply.github.com>
2023-08-07 16:46:59 -0400

    done

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,24 @@
 def badge_maker(name):
-    return None
+    return f'Hello, my name is {name}.'
 
 def batch_badge_creator(names):
-    return None
+    list = []
+    for name in names:
+        list.append(f'Hello, my name is {name}.')
+    return list
 
 def assign_rooms(names):
-    return None
+    list = []
+    num = 1
+    for name in names:
+        list.append(f'Hello, {name}! You\'ll be assigned to room {num}!')
+        num += 1
+    return list
 
 def printer(names):
-    return None
+    for badge in batch_badge_creator(names):
+         print(badge)
+    
+    for assignment in assign_rooms(names):
+         print(assignment)
+
